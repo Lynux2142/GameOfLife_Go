ab69dbe5ada2284f4bb4550feac4926291c754e0
Lucas Guillerot lguiller <lguiller@student.42.fr>
2025-12-19 11:02:41 +0100

    get screen size and run game of life in fullscreen

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 )
 
 const (
-	WINDOW_WIDTH  	= 1260
-	WINDOW_HEIGHT 	= 960
-	CELL_SIZE	 	= 1
+	CELL_SIZE = 1
 )
 
 func main() {
-	game := NewGame(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
-	ebiten.SetWindowSize(game.Width, game.Height)
+	width, height := ebiten.Monitor().Size()
+	ebiten.SetWindowSize(width, height)
+	ebiten.SetFullscreen(true)
+	game := NewGame(width, height, CELL_SIZE)
 	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
 	}
